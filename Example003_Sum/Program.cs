609de7ac910bf829b3a168e8b08a647c9e0c7ef4
Ky3uH <aakuzin@gmail.com>
2022-12-07 15:32:41 +0300

    ex_1_control

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numA=3;
-int numB=7;
+﻿int numA=new Random().Next(1,10);
+int numB=new Random().Next(1,10);
 int res = numA+numB;
+Console.WriteLine(numA);
+Console.WriteLine(numB);
 Console.WriteLine(res);
